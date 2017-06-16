17e1b2834b6f1341521a3490c95d2b05fb7c7089
JuliaHolodiy <ulia.alekseevna@mail.ru>
2017-06-17 02:48:08 +0300

    register OK

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import view
+
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'HotelApp.views.home', name='home'),
+    url(r'^$', 'HotelApp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^$', 'MyProject.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^register/', include('registr.urls')),
+    url(r'^register/$', view.RegisterFormView.as_view()),
     url(r'^news/', include('news.urls', namespace='news')),
     # url(r'^admin/', include(admin.site.urls)),
     # url(r'^register/$', RegisterFormView.as_view()),
-    # url(r'^login/$', LoginFormView.as_view()),
+    url(r'^login/$', view.LoginFormView.as_view()),
     # url(r'^exit/$', LoginFormView.as_view()),
 ]
--- MyProject/view.py
+++ MyProject/view.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from news.views import Post
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+# Функция для установки сессионного ключа.
+# По нему django будет определять, выполнил ли вход пользователь.
+from django.contrib.auth import login
 
 
 # можно переписать как в news/views.py
@@ -10,3 +16,37 @@ def home(request):
                 )
 
     return render_to_response('index.html', vars, context_instance=RequestContext(request))
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "login.html"
+
+    # В случае успеха перенаправим на главную.
+    success_url = "/"
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
